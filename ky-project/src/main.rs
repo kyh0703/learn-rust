00a3460237f8af71d3979998401da7e0ed9cc386
Overthinker <kyh0703@nate.com>
2026-02-09 18:03:01 +0900

    feat:add closure

--- ky-project/src/main.rs
+++ ky-project/src/main.rs
@@ -1,3 +1,22 @@
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    thread::spawn(move || println!("From thread: {:?}", list))
+        .join()
+        .unwrap();
+}
+
+#[test]
+fn iterator_demonstration() {
+    let vl = vec![1, 2, 3];
+
+    let mut vl_iter = vl.iter();
+
+    assert_ne!(vl_iter.next(), Some(&1));
+    assert_eq!(vl_iter.next(), Some(&2));
+    assert_eq!(vl_iter.next(), Some(&3));
+    assert_eq!(vl_iter.next(), None);
 }
